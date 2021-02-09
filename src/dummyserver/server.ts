c6077db815033084a8ee73cd07f8b10a09267537
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2021-02-09 10:17:38 +0800

    Changes from coding session

--- src/dummyserver/server.ts
+++ src/dummyserver/server.ts
@@ -6,6 +6,8 @@ import {
 	HoverParams,
 	InitializeResult
 } from 'vscode-languageserver/node';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 
 const connection = createConnection(ProposedFeatures.all);
 
@@ -20,9 +22,17 @@ connection.onInitialize((params: InitializeParams) => {
 	return result;
 });
 
-connection.onHover((q: HoverParams) => {
+connection.onHover((hoverRequest: HoverParams) => {
+	const fileContent = readFileSync(fileURLToPath(hoverRequest.textDocument.uri));
+	const fileAsString = fileContent.toString('utf8');
+	const fileLines = fileAsString.split('\n');
+	const selectedLine = fileLines[hoverRequest.position.line];
+	const tokens = selectedLine.split(/\s+/);
 	return {
-		contents: JSON.stringify(q)
+		contents: {
+			kind: 'markdown',
+			value: `Can't help with **${tokens[3]}**`,
+		},
 	};
 });
 

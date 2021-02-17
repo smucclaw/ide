c8a7950d8c0c0568ec524e2a30ce8fc30e8f956f
Andreas Källberg <anka.213@gmail.com>
2021-02-17 10:52:40 +0800

    Some random changes for use with haskell backend

--- src/dummyserver/server.ts
+++ src/dummyserver/server.ts
@@ -1,29 +1,36 @@
-
 import {
-	createConnection,
-	ProposedFeatures,
-	InitializeParams,
-	HoverParams,
-	InitializeResult
+  createConnection,
+  ProposedFeatures,
+  InitializeParams,
+  HoverParams,
+  InitializeResult,
 } from 'vscode-languageserver/node';
 
 const connection = createConnection(ProposedFeatures.all);
 
 connection.onInitialize((params: InitializeParams) => {
+  const result: InitializeResult = {
+    capabilities: {
+      hoverProvider: true,
+    },
+  };
+  console.log('Starting server');
 
-	const result: InitializeResult = {
-		capabilities: {
-			hoverProvider: true
-		}
-	};
-
-	return result;
+  return result;
 });
 
+// function throwErr(): never {
+//   throw new Error('Undefind');
+// }
+
 connection.onHover((q: HoverParams) => {
-	return {
-		contents: JSON.stringify(q)
-	};
+  connection.sendDiagnostics({
+    uri: q.textDocument.uri,
+    diagnostics: [{ range: { start: q.position, end: q.position }, message: 'Hello world' }],
+  });
+  return {
+    contents: JSON.stringify(q),
+  };
 });
 
 // Listen on the connection
